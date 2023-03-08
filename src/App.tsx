1593870360ec74b06d4f8fd7fd6e8f5eaaa6bd6f
Dinesh Sanala <dnsh4989@gmail.com>
2023-03-08 11:05:59 +0530

    page change on scroll functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,41 @@ function App() {
   const layer4: any = [useRef(""), useRef(""), useRef("")];
   const layer5: any = [useRef("")];
 
+  let [screenNumber, setScreenNumber] = useState(1);
+  let [isScrolling, setIsScrolling] = useState(false);
+
+  console.log(screenNumber);
+
+  const triggerFrameChange = (mode: string) => {
+    if (mode === "up") {
+      if (screenNumber > 1) {
+        setScreenNumber(screenNumber - 1);
+      }
+    } else if (mode === "down") {
+      setScreenNumber(screenNumber + 1);
+    }
+  };
+
+  const handleScroll = (event: any) => {
+    console.log(event);
+    event.stopPropagation();
+    if (isScrolling) {
+      return;
+    }
+    if (event.deltaY > 0) {
+      console.log("scrolling down");
+      triggerFrameChange("down");
+    }
+    if (event.deltaY < 0) {
+      console.log("scrolling up");
+      triggerFrameChange("up");
+    }
+    setIsScrolling(true);
+    setTimeout(() => {
+      setIsScrolling(false);
+    }, 1000);
+  };
+
   const handleMove = (event: any) => {
     handleLayer1(event);
     handleLayer2(event);
@@ -36,11 +71,15 @@ function App() {
   };
 
   const handleLayer2 = (event: any) => {
-    const x0 = 17 + event.clientX / 650;
+    const x0 = 32 - event.clientX / 1050;
 
-    const y0 = 17 - event.clientY / 650;
+    const y0 = 34 - event.clientY / 1050;
 
-    layer2[0].current.style.right = x0 + "rem";
+    if (!layer2[0].current) {
+      return;
+    }
+
+    layer2[0].current.style.left = x0 + "rem";
 
     layer2[0].current.style.top = y0 + "rem";
   };
@@ -56,13 +95,13 @@ function App() {
   };
 
   const handleLayer4 = (event: any) => {
-    const x0 = 20 + event.clientX / 4200;
-    const x1 = 24 + event.clientX / 4200;
-    const x2 = 2 + event.clientX / 4200;
+    const x0 = 20 + event.clientX / 3500;
+    const x1 = 24 + event.clientX / 3500;
+    const x2 = 2 + event.clientX / 3500;
 
-    const y0 = -6 - event.clientY / 4200;
-    const y1 = 2 - event.clientY / 4200;
-    const y2 = -5 - event.clientY / 4200;
+    const y0 = -6 - event.clientY / 3500;
+    const y1 = 2 - event.clientY / 3500;
+    const y2 = -5 - event.clientY / 3500;
 
     layer4[0].current.style.right = x0 + "rem";
     layer4[1].current.style.right = x1 + "rem";
@@ -84,7 +123,11 @@ function App() {
   };
 
   return (
-    <div className="main-container" onMouseMove={handleMove}>
+    <div
+      className="main-container"
+      onMouseMove={handleMove}
+      onWheelCapture={handleScroll}
+    >
       <div className="navigation">
         <input
           type="checkbox"
@@ -136,23 +179,36 @@ function App() {
         <div className="row">
           <div className="col-1-of-4">
             <div className="header__text-box">
-              <h1 className="heading-primary main-title">
-                <span className="heading-primary--main">
-                  DineSh <br />
-                  SanaLa
-                </span>
-                <div className="red-line-seperation">
-                  <span className="red-line"></span>
-                  <span className="red-line2"></span>
-                </div>
-                <span className="heading-primary--sub">
-                  Web Developer / <br /> FrontEnd Engineer.
-                </span>
-              </h1>
-
-              {/* <a href="#section-tours" className="btn btn--white btn--animated">
-                Discover our tours
-              </a> */}
+              {screenNumber === 1 && (
+                <h1 className="heading-primary main-title">
+                  <span className="heading-primary--main">
+                    DineSh <br />
+                    SanaLa
+                  </span>
+                  <div className="red-line-seperation">
+                    <span className="red-line"></span>
+                    <span className="red-line2"></span>
+                  </div>
+                  <span className="heading-primary--sub">
+                    Web Developer / <br /> FrontEnd Engineer.
+                  </span>
+                </h1>
+              )}
+              {screenNumber === 2 && (
+                <h1 className="heading-primary main-title">
+                  <span className="heading-primary--main font-size-6">
+                    Previous <br />
+                    Works
+                  </span>
+                  <div className="red-line-seperation">
+                    <span className="red-line"></span>
+                    <span className="red-line2"></span>
+                  </div>
+                  <span className="heading-primary--sub">
+                    Projects / <br /> Web Applications.
+                  </span>
+                </h1>
+              )}
             </div>
           </div>
           <div className="col-3-of-4">
@@ -162,51 +218,71 @@ function App() {
                   <div className="circle-3 circle-it">
                     <div className="circle-4 circle-it">
                       <div className="circle-5 circle-it">
-                        <div className="circle-6 circle-it">
-                          <div className="moon-white">
-                            <div className="crate-1"></div>
-                            <div className="crate-2"></div>
-                            <div className="crate-3"></div>
+                        {screenNumber === 1 && (
+                          <div className="circle-6 circle-it">
+                            <div className="moon-white">
+                              <div className="crate-1"></div>
+                              <div className="crate-2"></div>
+                              <div className="crate-3"></div>
+                            </div>
+                            <div className="cloud-4" ref={layer4[0]}></div>
+                            <div className="cloud-5" ref={layer4[1]}></div>
+                            <div className="cloud-6" ref={layer4[2]}></div>
                           </div>
-                          <div className="cloud-4" ref={layer4[0]}></div>
-                          <div className="cloud-5" ref={layer4[1]}></div>
-                          <div className="cloud-6" ref={layer4[2]}></div>
-                          <h1 className="heading-primary--red" ref={layer2[0]}>
-                            PORTFOLIO
-                          </h1>
-                        </div>
+                        )}
+                        {screenNumber === 2 && (
+                          <div className="circle-6 circle-it">
+                            <div className="row section2">
+                              <div className="col-1-of-2">Test 1</div>
+                              <div className="col-1-of-2">Test 2</div>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="cloud-1" ref={layer1[0]}></div>
-              <div className="cloud-2" ref={layer1[1]}></div>
-              <div className="cloud-3" ref={layer1[2]}></div>
-              <div className="dots-container" ref={layer5[0]}>
-                <div className="dots dot-1"></div>
-                <div className="dots dot-2"></div>
-                <div className="dots dot-3"></div>
-                <div className="dots dot-4"></div>
-                <div className="dots dot-5"></div>
-                <div className="dots dot-6"></div>
-                <div className="dots dot-7"></div>
-                <div className="dots dot-8"></div>
-                <div className="dots dot-9"></div>
-                <div className="dots dot-10"></div>
-                <div className="dots dot-11"></div>
-                <div className="dots dot-12"></div>
-                <div className="dots dot-13"></div>
-                <div className="dots dot-14"></div>
-                <div className="dots dot-15"></div>
-                <div className="dots dot-16"></div>
-                <div className="dots dot-17"></div>
-                <div className="dots dot-18"></div>
-                <div className="dots dot-19"></div>
-                <div className="dots dot-20"></div>
-                <div className="dots dot-21"></div>
-                <div className="dots dot-22"></div>
-              </div>
+              {screenNumber === 1 && (
+                <h1 className="heading-primary--red" ref={layer2[0]}>
+                  PORTFOLIO
+                </h1>
+              )}
+              {screenNumber === 1 && (
+                <>
+                  <div className="cloud-1" ref={layer1[0]}></div>
+                  <div className="cloud-2" ref={layer1[1]}></div>
+                  <div className="cloud-3" ref={layer1[2]}></div>
+                </>
+              )}
+              {screenNumber === 1 && (
+                <>
+                  <div className="dots-container" ref={layer5[0]}>
+                    <div className="dots dot-1"></div>
+                    <div className="dots dot-2"></div>
+                    <div className="dots dot-3"></div>
+                    <div className="dots dot-4"></div>
+                    <div className="dots dot-5"></div>
+                    <div className="dots dot-6"></div>
+                    <div className="dots dot-7"></div>
+                    <div className="dots dot-8"></div>
+                    <div className="dots dot-9"></div>
+                    <div className="dots dot-10"></div>
+                    <div className="dots dot-11"></div>
+                    <div className="dots dot-12"></div>
+                    <div className="dots dot-13"></div>
+                    <div className="dots dot-14"></div>
+                    <div className="dots dot-15"></div>
+                    <div className="dots dot-16"></div>
+                    <div className="dots dot-17"></div>
+                    <div className="dots dot-18"></div>
+                    <div className="dots dot-19"></div>
+                    <div className="dots dot-20"></div>
+                    <div className="dots dot-21"></div>
+                    <div className="dots dot-22"></div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
